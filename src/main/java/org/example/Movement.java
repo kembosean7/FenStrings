cd869cb7e62fa582a1e9293b0d935213d22dc05a
Sean Kembo <kembosean7@gmail.com>
2024-12-30 18:35:31 +0200

    adding validation for color, throw error if color is invalid

--- src/main/java/org/example/Movement.java
+++ src/main/java/org/example/Movement.java
@@ -2,7 +2,7 @@ package org.example;
 import java.util.Arrays;
 
 public class Movement {
-    public String moveTurn(String fen){
+    public String moveTurn(String fen) throws IllegalArgumentException{
         String[] parts = fen.split(" ");
         String color = parts[1];
         String output = null;
@@ -11,12 +11,12 @@ public class Movement {
             output = "White to move";
         }else if(color.equalsIgnoreCase("b")){
             output = "Black to move";
-        }
+        }else throw new IllegalArgumentException("Invalid color input: expected 'w' for White or 'b' for Black");
         return output;
     }
 
     public static void main(String[] args){
         Movement move = new Movement();
-        System.out.println(move.moveTurn( "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1"));
+        System.out.println(move.moveTurn( "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR k KQkq - 0 1"));
     }
 }
