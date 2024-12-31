80d0c1a9ac98ad6d4fc79906dfddc9a89174793b
Sean Kembo <kembosean7@gmail.com>
2024-12-31 16:11:16 +0200

    coverting list of board into string format in board class

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -4,32 +4,41 @@ import java.util.List;
 
 
 public class Board {
-    public List<List<String>>  generateBoard(String fen){
+    public String generateBoard(String fen) {
 
         String[] parts = fen.split(" ");
         String board = parts[0];
         List<List<String>> boardList = new ArrayList<>();
         String[] rows = board.split("/");
+        StringBuilder output = new StringBuilder();
 
-        for(String row: rows){
+
+
+        for (String row : rows) {
             List<String> boardRow = new ArrayList<>();
-            for(int i = 0; i < row.length(); i++){
+            for (int i = 0; i < row.length(); i++) {
                 char element = row.charAt(i);
-                if(Character.isDigit(element)){
+                if (Character.isDigit(element)) {
                     int emptySpaces = Character.getNumericValue(element);
-                    for (int j = 0; j < emptySpaces; j++){
+                    for (int j = 0; j < emptySpaces; j++) {
                         boardRow.add(".");
                     }
-                }else{
+                } else {
                     boardRow.add(String.valueOf(element));
 
                 }
             }
-            boardList.add(boardRow);;
+            output.append(String.join(" ", boardRow)).append("\n");
+
+
         }
-        return boardList;
+        return output.toString();
 
 
+    }
+    public static void main(String[] args){
+        Board board = new Board();
+        System.out.print(board.generateBoard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQk - 0 1"));
     }
 
 };
--- src/main/java/org/example/EnPasantSquare.java
+++ src/main/java/org/example/EnPasantSquare.java
@@ -1,4 +1,16 @@
 package org.example;
 
 public class EnPasantSquare {
-}
+    public String enpasantsquare(String fen){
+
+        String[] parts = fen.split(" ");
+        String enPasant = parts[3];
+        if(enPasant.equals("-")){
+            return "No en passant square";
+        }
+        else{
+            return "En passant square: " + enPasant;
+        }
+    }
+
+};
